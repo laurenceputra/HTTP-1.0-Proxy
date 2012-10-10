7411fc019691667a19f6220261e581eaff2a604f
laurenceputra <laurenceputra@gmail.com>
2012-10-10 14:40:25 +0800

    fails 443 gracefully now

--- Connection.cpp
+++ Connection.cpp
@@ -61,68 +61,73 @@ void Connection::thread_process(){
         //split url into host and resource
         pos = url.find("//");
         host = url.substr(pos + 2);
-        pos = host.find("/");
-        host.erase(pos);
-
-        second_io_service = boost::shared_ptr<boost::asio::io_service>(new boost::asio::io_service);
-        boost::asio::ip::tcp::resolver resolver(*second_io_service);
-        boost::asio::ip::tcp::resolver::query query(host, "80");
-        boost::asio::ip::tcp::resolver::iterator endpoint_iterator;
-        try{
-            endpoint_iterator = resolver.resolve(query);
+        if(pos == std::string::npos){
+            handle_error("405");
         }
-        catch(std::exception& e){
-            std::cout << "Exception2: " << e.what() << std::endl;
-            handle_error("404");
-            quit = true;
-        }
+        else{ 
+            pos = host.find("/");
+            host.erase(pos);
 
-        if(!quit){
-            boost::asio::ip::tcp::resolver::iterator end;
-            second_socket = boost::shared_ptr<boost::asio::ip::tcp::socket>(new boost::asio::ip::tcp::socket(*second_io_service));
-            boost::system::error_code _error = boost::asio::error::host_not_found;
-            while(_error && endpoint_iterator != end) {
-                second_socket->close();
-                second_socket->connect(*endpoint_iterator++, _error);
+            second_io_service = boost::shared_ptr<boost::asio::io_service>(new boost::asio::io_service);
+            boost::asio::ip::tcp::resolver resolver(*second_io_service);
+            boost::asio::ip::tcp::resolver::query query(host, "80");
+            boost::asio::ip::tcp::resolver::iterator endpoint_iterator;
+            try{
+                endpoint_iterator = resolver.resolve(query);
+            }
+            catch(std::exception& e){
+                std::cout << "Exception2: " << e.what() << std::endl;
+                handle_error("404");
+                quit = true;
             }
 
-            output_stream = "GET ";
-            output_stream += url;
-            output_stream += " HTTP/1.0\r\n\r\n";
-            boost::asio::write(*second_socket, boost::asio::buffer(output_stream));
-
-            //read headers
-            input_stream = "";
-            while(input_stream.find("\r\n\r\n") == std::string::npos){
-                (*second_socket).read_some(boost::asio::buffer(input_buffer, 1));
-                input_stream += input_buffer[0];
-            }
-            boost::asio::write(client_socket, boost::asio::buffer(input_stream));
-
-            //get content length
-            pos = input_stream.find("Content-Length: ");
-            content_length = input_stream.substr(pos + 16);
-
-            pos = content_length.find("\r\n");
-            content_length.erase(pos);
-            int_content_length = atoi(content_length.data());
-            if(int_content_length == 0){
-                int_content_length = -1;
-            }
-            length = 512;
-            //read content
-            while(!quit){
-                try{
-                    length = (*second_socket).read_some(boost::asio::buffer(input_buffer, 512));
-                    input_stream = std::string(input_buffer, length);
-                    boost::asio::write(client_socket, boost::asio::buffer(input_stream, length));
+            if(!quit){
+                boost::asio::ip::tcp::resolver::iterator end;
+                second_socket = boost::shared_ptr<boost::asio::ip::tcp::socket>(new boost::asio::ip::tcp::socket(*second_io_service));
+                boost::system::error_code _error = boost::asio::error::host_not_found;
+                while(_error && endpoint_iterator != end) {
+                    second_socket->close();
+                    second_socket->connect(*endpoint_iterator++, _error);
                 }
-                catch(std::exception& e){
-                    std::cout << "Exception3: " << e.what() << std::endl;
-                    quit = true;
+
+                output_stream = "GET ";
+                output_stream += url;
+                output_stream += " HTTP/1.0\r\n\r\n";
+                boost::asio::write(*second_socket, boost::asio::buffer(output_stream));
+
+                //read headers
+                input_stream = "";
+                while(input_stream.find("\r\n\r\n") == std::string::npos){
+                    (*second_socket).read_some(boost::asio::buffer(input_buffer, 1));
+                    input_stream += input_buffer[0];
+                }
+                boost::asio::write(client_socket, boost::asio::buffer(input_stream));
+
+                //get content length
+                pos = input_stream.find("Content-Length: ");
+                content_length = input_stream.substr(pos + 16);
+
+                pos = content_length.find("\r\n");
+                content_length.erase(pos);
+                int_content_length = atoi(content_length.data());
+                if(int_content_length == 0){
+                    int_content_length = -1;
+                }
+                length = 512;
+                //read content
+                while(!quit){
+                    try{
+                        length = (*second_socket).read_some(boost::asio::buffer(input_buffer, 512));
+                        input_stream = std::string(input_buffer, length);
+                        boost::asio::write(client_socket, boost::asio::buffer(input_stream, length));
+                    }
+                    catch(std::exception& e){
+                        std::cout << "Exception3: " << e.what() << std::endl;
+                        quit = true;
+                    }
                 }
             }
-        } 
+        }   
     }
     client_socket.close();
 }
